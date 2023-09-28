13fd82d783d4bc1eadc8799d68c399fd5747a6dd
PabloAlda <pabloam0309@gmail.com>
2023-09-28 12:35:13 +0200

    Create temperatura.py

---
+++ temperatura.py
@@ -0,0 +1,4 @@
+temperatura_celsius = 21.5
+print("La temperatura en Celsius es:", temperatura_celsius)
+temperatura_fahrenheit = (temperatura_celsius * 9/5) + 32
+print("La temperatura en Fahrenheit es:", temperatura_fahrenheit)
